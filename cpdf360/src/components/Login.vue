43e2cf9f63d593cafeb84a033bd3608c25a78b35
xiaoyu_wu <xiaoyu_wu@foxitsoftware.cn>
2021-03-03 22:05:23 +0800

    feat:begin

---
+++ cpdf360/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+};
+</script>
+<style>
+.login{
+    box-sizing: border-box;
+    background: url('../assets/bg.png') center center no-repeat;
+    background-size: 100% 100%;
+}
+</style>
